ecf70b58a9ea5014b4c55ec0289b787d859b9e3a
Ranjan35gupta <Ranjan35gupta@gmail.com>
2023-04-19 23:47:01 +0530

    created the first project and updated some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <img src={logo} className="App-logo" alt="logo" />
+      <header class="App-header"><h1>Benefits of using react...</h1></header>
+      <section class="App-header"><p>1.Components-based architecture</p>
+      <p>2.Virtual DOM for efficient updates</p>
+      <p>3.Rich ecosystem and community</p>
+      <p>4.Cross-platform development</p>
+      <p>5.Strong community support:</p></section>
+      
+      
+      <button>Get Started...</button>
+
+
+        
+      
     </div>
   );
 }
